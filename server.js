7a276c7b1d6c19f365d065c0c61ae5132b362d74
Seyitumturk <seyitumturk@gmail.com>
2023-01-23 19:04:22 -0400

    First Commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const app = express()
+const methodOverride = require('method-override')
+
+mongoose.connect('mongodb://localhost/blog')
+mongoose.set('strictQuery', true);
+
+
+app.set('view engine', 'ejs')
+
+// Allowing express server to recieve our form parameters.
+app.use(express.urlencoded({
+    extended: false
+}))
+
+app.use(methodOverride('_method'))
+
+
+app.get('/', async (req, res) => {
+
+    const articles = await Article.find().sort({
+        createdAt: 'desc'
+    })
+    res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(5000)
